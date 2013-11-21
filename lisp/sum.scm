6e9172d99eab61e46f1140a768eae159abfbe3b0
fuli <daniel820313@gmail.com>
2013-11-21 16:17:47 +0800

    update sum

--- lisp/sum.scm
+++ lisp/sum.scm
@@ -1,6 +1,18 @@
-(define (sum-int a b) 
+(define (sum term a next b)
 	(if (> a b)
 		0
-		(+ a (sum-int (+ a 1) b))
+		(+ (term a) (sum term (next a) next b))
 	)
+)
+
+(define (sum-int a b)
+	(define (term n) n)
+	(define (next n) (+ n 1))
+	(sum term a next b)
+)
+
+(define (sum-square a b)
+	(define (term n) (* n n))
+	(define (next n) (+ n 1))
+	(sum term a next b)
 )
