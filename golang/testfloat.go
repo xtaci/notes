f73ef66da183d89ad3aec849c3e3b160640e0530
fuli <daniel820313@gmail.com>
2013-11-24 12:38:16 +0800

    update

--- golang/testfloat.go
+++ golang/testfloat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"strconv"
 	"fmt"
 )
 func main() {
@@ -12,4 +13,18 @@ func main() {
 		fmt.Println(reflect.TypeOf(Y*1.1))
 		fmt.Println(reflect.TypeOf(x))
 	}
+
+	f, err := strconv.ParseFloat("0", 32)
+	fmt.Println(f, err)
+
+	f1 := float32(0)
+	f2 := float32(0)
+	fmt.Println(f1/f2 < 0)
+
+	testx := float32(90000000000)
+	fmt.Println(testx)
+
+	d1, err := strconv.ParseFloat("0.2", 32)
+	d2, err := strconv.ParseFloat("0.4", 32)
+	fmt.Println(d1,d2,d1+d2)
 }
