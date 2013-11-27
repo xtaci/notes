dd5f7ed9826672cbcd7a2b9ef9f837b83dae6084
fuli <daniel820313@gmail.com>
2013-11-27 11:52:41 +0800

    update

--- golang/testfmt.go
+++ golang/testfmt.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"fmt"
+	"strings"
 )
 
 func DEBUG(v ...interface{}) {
-    log.Printf("\033[1;33m[DEBUG] %v \033[0m\n", fmt.Sprint(v...))
+    log.Printf("\033[1;33m[DEBUG] %v \033[0m\n", strings.TrimRight(fmt.Sprintln(v...),"\n"))
 }
 
 func main() {
 	DEBUG("XX","DX")
 	DEBUG("1","2")
+	DEBUG("weapon", "level", 18, "armor_level",18)
+	fmt.Println("weapon", "level", 18, "armor_level",18)
 }
