6f5ed32feb8a7f0b9b65782cce609467a83174f7
fuli <daniel820313@gmail.com>
2013-12-17 17:48:32 +0800

    update lambda

--- golang/testlambda.go
+++ golang/testlambda.go
@@ -1,15 +1,24 @@
 package main
 
+import (
+	"time"
+)
+
 func fun(f func()) {
 	for i := 0; i < 10; i++ {
 		f()
 	}
 }
 
-func main() {
+func fun2() {
 	x := 1
-	fun(func() {
+	go fun(func() {
 		x++
 		println(x)
 	})
+}
+
+func main() {
+	fun2()
+	time.Sleep(10)
 }
