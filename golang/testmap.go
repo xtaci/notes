a69105ef77b06094b8015e844ad196517fde48b6
fuli <daniel820313@gmail.com>
2014-02-17 16:15:39 +0800

    update

--- golang/testmap.go
+++ golang/testmap.go
@@ -8,8 +8,16 @@ type X struct {
 	Data map[string]string
 }
 
+var tmp map[string]interface{}
+
 func main() {
+	tmp = make(map[string]interface{})
 	x := &X{}
 	x.Data["A"] = "B"
 	fmt.Println(x)
+
+	tmp["a"] = 1
+	tmp["b"] = 2
+
+	fmt.Println(tmp["a"] + tmp["b"])
 }
