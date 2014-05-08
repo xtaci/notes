caae02ec199827346bc4719d6319920be27570ed
fuli <daniel820313@gmail.com>
2014-05-08 16:03:39 +0800

    a deadlock simul for lock in sig handler

---
+++ tlpi/deadlock.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+#include <stdint.h>
+
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+void sig_handler(int);
+
+int main(void) {
+	// init signal
+	signal(SIGUSR1, sig_handler);
+
+	// lock
+	pthread_mutex_lock(&mtx);
+	int64_t i = 0;
+	for (;;) {
+		printf("%ld\r", i++);
+	}	
+	pthread_mutex_unlock(&mtx);
+}
+
+void sig_handler(int sig) {
+	printf("signal handler entered %d\n",sig);
+	pthread_mutex_lock(&mtx);
+	printf("signal handler mutex entered %d\n", sig);
+	pthread_mutex_unlock(&mtx);
+}
