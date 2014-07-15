2cf26b90d1fca20c357f42da02f4deaf20acfe7f
fuli <daniel820313@gmail.com>
2014-07-15 14:01:33 +0800

    update

--- golang/testslice3.go
+++ golang/testslice3.go
@@ -8,6 +8,7 @@ func main() {
 	xx := []int32{10, 20, 30, 40, 12, 3, 4, 5, 5}
 	a := xx[:2]
 	b := xx[:3]
-	fmt.Println(cap(a), cap(b))
+	c := xx[3:]
+	fmt.Println(cap(a), cap(b), cap(c))
 	fmt.Println(a[:])
 }
