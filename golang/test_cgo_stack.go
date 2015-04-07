836535a3d85b9bb62de2cc32ebd314d43b63021e
xtaci <daniel820313@gmail.com>
2015-04-07 13:20:00 +0800

    update

---
+++ golang/test_cgo_stack.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+#include "stdio.h"
+
+void test(int n) {
+	char dummy[1024];
+
+	printf("in c test func iterator %d\n", n);
+	if(n <= 0) {
+		return;
+	}
+	dummy[n] = '\a';
+	test(n-1);
+}
+#cgo CFLAGS: -g
+*/
+import "C"
+
+func main() {
+	C.test(C.int(20))
+}
