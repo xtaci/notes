fbdd6795b17f12f220496fdf527b97e26059eb59
xtaci <daniel820313@gmail.com>
2015-04-15 16:40:26 +0800

    update

---
+++ crackcode/1.4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool anagram(char * restrict s1, int n1, char * restrict s2, int n2);
+
+int
+main(void) {
+	char s1[] = "mary";
+	char s2[] = "army";
+	char s3[] = "mami";
+	printf("%d\n", anagram(s1, sizeof(s1)-1, s2, sizeof(s2)-1));
+	printf("%d\n", anagram(s1, sizeof(s1)-1, s3, sizeof(s3)-1));
+}
+
+bool
+anagram(char * restrict s1, int n1, char * restrict s2, int n2) {
+	if (n1!=n2) {
+		return false;
+	}
+
+	for (int i=0;i<n1;i++){
+		for (int j=0;j<n2;j++) {
+			if (s1[i] == s2[j]) {
+				s2[j] =0;
+			}
+		}
+	}
+
+	for (int j=0;j<n2;j++) {
+		if(s2[j] >0) return false;
+	}
+	return true;
+}
