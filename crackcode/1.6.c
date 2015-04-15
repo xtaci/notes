80efe376d83fbfdc6730c98883847c1bb9f2ed85
xtaci <daniel820313@gmail.com>
2015-04-15 17:42:27 +0800

    add matrix rotate

---
+++ crackcode/1.6.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+const int N = 4;
+
+void print_m(int m[][N]);
+void rotate_m(int m[][N]);
+
+int
+main(void) {
+	int m[N][N];
+	for (int i=0;i<N;i++) {
+		for (int j=0;j<N;j++) {
+			m[i][j] = i*j;
+		}
+	}
+	print_m(m);
+	rotate_m(m);
+	print_m(m);
+}
+
+void
+print_m(int m[][N]) {
+	for (int i=0;i<N;i++) {
+		printf("%d %d %d %d\n", m[i][0], m[i][1],m[i][2], m[i][3]);
+	}
+}
+
+
+void
+rotate_m(int m[][N]) {
+	for (int i=0;i<N;i++) {
+		for (int j=0;j<N-i;j++) {
+			int b = m[i][j];
+			m[i][j] = m[N-j-1][N-i-1];
+			m[N-j-1][N-i-1] = b;
+		}
+	}
+}
