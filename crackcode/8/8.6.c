eb185b789e634c69f6372eb9ff393a86e1eacb76
xtaci <daniel820313@gmail.com>
2015-04-20 13:52:09 +0800

    update

--- crackcode/8/8.6.c
+++ crackcode/8/8.6.c
@@ -3,34 +3,35 @@
 
 const int N = 10;
 enum Color {
+	Black = 0,
 	Green = 1
 };
 
-void fill(int screen[][N], int x, int y, enum Color color);
+void fill(int screen[][N], int x, int y, enum Color ocolor, enum Color ncolor);
 void print_m(int m[][N]);
 
 int 
 main(void) {
 	int screen[N][N];
-	memset(screen ,0, sizeof(screen));
+	memset(screen ,Black, sizeof(screen));
 	printf("sizeof 2d-array %ld\n", sizeof(screen));
 	print_m(screen);
-	fill(screen ,N-1, N-1, Green);
+	fill(screen ,N-1, N-1, screen[N-1][N-1], Green);
 	print_m(screen);
 }
 
-void
-fill(int screen[][N], int x, int y, enum Color color) {
+void 
+fill(int screen[][N], int x, int y, enum Color ocolor, enum Color ncolor) {
 	if (x <0 || x > N || y <0 || y > N) {
 		return;
 	}
 
-	if (screen[y][x] == 0) {
-		screen[y][x] = color;
-		fill(screen, x-1, y, color);
-		fill(screen, x+1, y, color);
-		fill(screen, x, y-1, color);
-		fill(screen, x, y+1, color);
+	if (screen[y][x] == ocolor) {
+		screen[y][x] = ncolor;
+		fill(screen, x-1, y, ocolor, ncolor);
+		fill(screen, x+1, y, ocolor, ncolor);
+		fill(screen, x, y-1, ocolor, ncolor);
+		fill(screen, x, y+1, ocolor, ncolor);
 	}
 }
 
