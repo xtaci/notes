5e47a4291dd3c0a80b2523de2124a2e06ff52016
xtaci <daniel820313@gmail.com>
2016-03-08 18:38:32 +0800

    update

--- haskell/tut0.hs
+++ haskell/tut0.hs
@@ -10,9 +10,6 @@ nats = 1 : map (+1) nats
 removeNonUppercase :: [Char] -> [Char]  
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]   
 
-factorial :: Integer -> Integer  
-factorial n = product [1..n]  
-
 circumference :: Float -> Float  
 circumference r = 2 * pi * r  
 
@@ -20,3 +17,15 @@ circumference r = 2 * pi * r
 circumference' :: Double -> Double  
 circumference' r = 2 * pi * r  
 
+
+charName :: Char -> String  
+charName 'a' = "Albert"  
+charName 'b' = "Broseph"  
+charName 'c' = "Cecil"  
+
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1)  
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)  
