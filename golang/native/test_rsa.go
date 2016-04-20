8db05737255dea119af8e9c933712468fc030188
xtaci <daniel820313@gmail.com>
2016-04-20 10:49:51 +0800

    update

--- golang/native/test_rsa.go
+++ golang/native/test_rsa.go
@@ -7,10 +7,19 @@ import (
 )
 
 func main() {
-
-	priv, err := rsa.GenerateKey(rand.Reader, 1024)
+	priv, err := rsa.GenerateKey(rand.Reader, 256)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%#v", priv)
+	fmt.Printf("%#v\n", priv)
+	msg := []byte("hello")
+	out, err := rsa.EncryptPKCS1v15(rand.Reader, &priv.PublicKey, msg)
+	if err != nil {
+		panic(err)
+	}
+	plaintext, err := rsa.DecryptPKCS1v15(nil, priv, out)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(plaintext))
 }
