5e8e70d97aecb2a3756bb53dc84df3bb7950f0f5
xtaci <daniel820313@gmail.com>
2016-12-03 14:08:43 +0800

    upd

---
+++ golang/kafka/kafka_producer.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	. "github.com/Shopify/sarama"
+)
+
+func main() {
+	producer, err := NewAsyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// Trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	var enqueued, errors int
+ProducerLoop:
+	for {
+		select {
+		case producer.Input() <- &ProducerMessage{Topic: "test", Key: StringEncoder("userid-1"), Value: StringEncoder("testing 123")}:
+			enqueued++
+		case err := <-producer.Errors():
+			log.Println("Failed to produce message", err)
+			errors++
+		case <-signals:
+			break ProducerLoop
+		}
+	}
+
+	log.Printf("Enqueued: %d; errors: %d\n", enqueued, errors)
+}
