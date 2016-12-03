fd60fd9f9ce9b658497a9c59a3e788b19bb953f1
xtaci <daniel820313@gmail.com>
2016-12-03 14:18:23 +0800

    upd

---
+++ golang/kafka/sync_producer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	. "github.com/Shopify/sarama"
+)
+
+func main() {
+	producer, err := NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	msg := &ProducerMessage{Topic: "test", Key: StringEncoder("userid-1"), Value: StringEncoder("testing 123")}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("FAILED to send message: %s\n", err)
+	} else {
+		log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+	}
+}
