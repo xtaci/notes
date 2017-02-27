c22dd0aeea822ede7f338599b2dcdd753e5a1e7b
xtaci <daniel820313@gmail.com>
2017-02-27 12:29:36 +0800

    upd

--- golang/strings/testtrim.go
+++ golang/strings/testtrim.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("r:", strings.TrimRight("abcdefedcba", "ab"))
+	fmt.Println("r:", strings.TrimRight("abcdefedcba", "fedcba"))
 	fmt.Println("l:", strings.TrimLeft("abcdefedcba", "abdef"))
 }
