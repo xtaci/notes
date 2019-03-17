64cee52e61c8914f09a43f88d4d052cb5ea1851a
xtaci <daniel820313@gmail.com>
2019-03-18 02:38:22 +0800

    precise control of sorting memory by using bi-directional write

--- other/v3/strfind_v3.go
+++ other/v3/strfind_v3.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"container/heap"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
@@ -11,39 +13,119 @@ import (
 	"sort"
 	"strconv"
 	"strings"
-	"unsafe"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
 // pre-processing stage
 // an in-memory sorter
+
+// rawEntry binary format:
+// ord int64
+// bytesSize int32
+// bytesPtr int32
+type rawEntry []byte
+
+func (e rawEntry) ord() int64              { return int64(binary.LittleEndian.Uint64(e[:])) }
+func (e rawEntry) sz() int32               { return int32(binary.LittleEndian.Uint32(e[8:])) }
+func (e rawEntry) ptr() int32              { return int32(binary.LittleEndian.Uint32(e[12:])) }
+func (e rawEntry) bytes(buf []byte) []byte { return buf[e.ptr():][:e.sz()] }
+func (e rawEntry) Len() int                { return 16 }
+
 type entry struct {
-	str string
+	str []byte
 	ord int64
-	cnt int64 // string count
 }
 
-// split large slice set into a group of small
-// sets to mitigate slow runtime.growslice ops
+// split large slice set into a group of small sets
+// for limiting memory usage, we write content forth to back
+// and write idx from back to forth, as:
+// [string,string, ...... idx100, idx99, idx1]
 type entrySet struct {
-	entries []entry
-	length  int
+	buf         []byte
+	idxWritten  int
+	dataWritten int
+	dataPtr     int // point to next writable place, as same below
+
+	idxPtr  int
+	idxSize int
+	idxMeta rawEntry // empty element
+	swapbuf []byte
 }
 
-func (s *entrySet) Len() int           { return s.length }
-func (s *entrySet) Less(i, j int) bool { return s.entries[i].str < s.entries[j].str }
-func (s *entrySet) Swap(i, j int)      { s.entries[i], s.entries[j] = s.entries[j], s.entries[i] }
+func newEntrySet(sz int) *entrySet {
+	e := new(entrySet)
+	e.buf = make([]byte, sz)
+	e.idxPtr = sz - e.idxMeta.Len()
+	e.swapbuf = make([]byte, e.idxMeta.Len())
+	return e
+}
 
+func (s *entrySet) Add(line []byte, ord int64) bool {
+	sz := len(line)
+	if s.idxWritten+s.dataWritten+sz+s.idxMeta.Len() >= len(s.buf) {
+		return false
+	}
+
+	// write data
+	copy(s.buf[s.dataPtr:], line)
+	// write idx
+	binary.LittleEndian.PutUint64(s.buf[s.idxPtr:], uint64(ord))
+	binary.LittleEndian.PutUint32(s.buf[s.idxPtr+8:], uint32(len(line)))
+	binary.LittleEndian.PutUint32(s.buf[s.idxPtr+12:], uint32(s.dataPtr))
+
+	// pointer update
+	s.dataWritten += sz
+	s.dataPtr += sz
+	s.idxWritten += s.idxMeta.Len()
+	s.idxPtr -= s.idxMeta.Len()
+	return true
+}
+
+// return the ith entry in binary form
+func (s *entrySet) e(i int) rawEntry {
+	return rawEntry(s.buf[len(s.buf)-s.idxMeta.Len()*(i+1):][:s.idxMeta.Len()])
+}
+
+// return the ith element in text form
+func (s *entrySet) get(i int) entry {
+	e := rawEntry(s.buf[len(s.buf)-s.idxMeta.Len()*(i+1):][:s.idxMeta.Len()])
+	return entry{e.bytes(s.buf), e.ord()}
+}
+
+func (s *entrySet) Len() int { return s.idxWritten / s.idxMeta.Len() }
+func (s *entrySet) Less(i, j int) bool {
+	return bytes.Compare(s.e(i).bytes(s.buf), s.e(j).bytes(s.buf)) < 0
+}
+
+func (s *entrySet) Swap(i, j int) {
+	copy(s.swapbuf, s.e(i))
+	copy(s.e(i), s.e(j))
+	copy(s.e(j), s.swapbuf)
+}
+
+// set reader
 type entrySetReader struct {
-	entries []entry
-	head    int
+	set  *entrySet
+	head int
+	elem entry
+}
+
+func newEntrySetReader(set *entrySet) *entrySetReader {
+	if set.Len() == 0 {
+		return nil
+	}
+	esr := new(entrySetReader)
+	esr.set = set
+	esr.elem = set.get(0)
+	return esr
 }
 
 func (esr *entrySetReader) next() bool {
 	esr.head++
-	if esr.head >= len(esr.entries) {
+	if esr.head >= esr.set.Len() {
 		return false
 	}
+	esr.elem = esr.set.get(esr.head)
 	return true
 }
 
@@ -54,7 +136,7 @@ type memSortAggregator struct {
 
 func (h *memSortAggregator) Len() int { return len(h.sets) }
 func (h *memSortAggregator) Less(i, j int) bool {
-	return h.sets[i].entries[h.sets[i].head].str < h.sets[j].entries[h.sets[j].head].str
+	return bytes.Compare(h.sets[i].elem.str, h.sets[j].elem.str) < 0
 }
 func (h *memSortAggregator) Swap(i, j int)      { h.sets[i], h.sets[j] = h.sets[j], h.sets[i] }
 func (h *memSortAggregator) Push(x interface{}) { h.sets = append(h.sets, x.(*entrySetReader)) }
@@ -67,97 +149,89 @@ func (h *memSortAggregator) Pop() interface{} {
 
 // words sorter for big memory
 type sortWords struct {
-	sets        []entrySet
-	nextElem    int64
-	setMemSize  int64 // memory size of a set
-	setSize     int64
-	stringUsage int64 // string memory usage track
-	setUsage    int64 // set memory usage track
-	limit       int64 // max total memory usage
+	sets    []*entrySet
+	setSize int
+	limit   int // max total memory usage
 }
 
-func (h *sortWords) Len() int { return int(h.nextElem) }
+func (h *sortWords) Len() int {
+	n := 0
+	for k := range h.sets {
+		n += h.sets[k].Len()
+	}
+	return n
+}
 
 func (h *sortWords) Serialize(w io.Writer) {
 	if len(h.sets) > 0 {
 		agg := new(memSortAggregator)
 		for k := range h.sets {
 			log.Println("sorting sets#", k)
-			sort.Sort(&h.sets[k])
-			heap.Push(agg, &entrySetReader{h.sets[k].entries[:h.sets[k].length], 0})
+			sort.Sort(h.sets[k])
+			heap.Push(agg, newEntrySetReader(h.sets[k]))
 		}
 		log.Println("merging sorted sets to file")
 
 		written := 0
 		esr := heap.Pop(agg).(*entrySetReader)
-		last := esr.entries[esr.head]
+		last := esr.elem
+		last_cnt := 1
 		if esr.next() {
 			heap.Push(agg, esr)
 		}
 
 		for agg.Len() > 0 {
 			esr = heap.Pop(agg).(*entrySetReader)
-			elem := &esr.entries[esr.head]
-			if elem.str == last.str { // condense output
-				last.cnt += elem.cnt
+			elem := esr.elem
+			if bytes.Compare(elem.str, last.str) == 0 { // condense output
+				last_cnt++
 			} else {
-				fmt.Fprintf(w, "%v,%v,%v\n", last.str, last.ord, last.cnt)
-				last = *elem
+				fmt.Fprintf(w, "%v,%v,%v\n", string(last.str), last.ord, last_cnt)
+				last = elem
+				last_cnt = 1
 				written++
 			}
 			if esr.next() {
 				heap.Push(agg, esr)
 			}
 		}
-		fmt.Fprintf(w, "%v,%v,%v\n", last.str, last.ord, last.cnt)
+		fmt.Fprintf(w, "%v,%v,%v\n", string(last.str), last.ord, last_cnt)
 		written++
 		log.Println("written", written, "elements")
-
-		h.nextElem = 0
 		h.sets = nil
-		h.setUsage = 0
-		h.stringUsage = 0
 	}
 }
 
 // Add controls the memory for every input
 func (h *sortWords) Add(line []byte, ord int64) bool {
-	if h.nextElem%h.setSize == 0 { // create new set
-		if h.setUsage+h.stringUsage+h.setMemSize > h.limit { // check new set creation
+	if h.sets == nil { // init first one
+		h.sets = []*entrySet{newEntrySet(h.setSize)}
+	}
+
+	set := h.sets[len(h.sets)-1]
+	if !set.Add(line, ord) {
+		if h.setSize*(len(h.sets)+1) > h.limit { // limit reached
 			return false
 		}
-		entries := make([]entry, h.setSize)
-		h.sets = append(h.sets, entrySet{entries, 0})
-		h.setUsage += h.setMemSize
+		newSet := newEntrySet(h.setSize)
+		h.sets = append(h.sets, newSet)
+		newSet.Add(line, ord)
 	}
-
-	if h.setUsage+h.stringUsage+int64(len(line)) > h.limit { // check new(string)
-		return false
-	}
-
-	h.stringUsage += int64(len(line))
-	sidx := h.nextElem / h.setSize
-	eidx := h.nextElem % h.setSize
-	h.sets[sidx].entries[eidx] = entry{string(line), ord, 1}
-	h.sets[sidx].length++
-	h.nextElem++
 	return true
 }
 
-func (h *sortWords) init(limit int64) {
-	e := entry{}
-	h.setSize = 1 << 20 // single set limited to 1M elements
-	h.setMemSize = int64(unsafe.Sizeof(e)) * h.setSize
+func (h *sortWords) init(limit int) {
+	h.setSize = 1 << 24 // 16MB set
 	h.limit = limit
-	if h.limit < h.setMemSize {
-		h.limit = 2 * h.setMemSize
+	if h.limit < h.setSize {
+		h.limit = h.setSize
 	}
 }
 
 // sort2Disk writes strings with it's ordinal and count
 // xxxxx,1234,1
 // aaaa,5678,10
-func sort2Disk(r io.Reader, memLimit int64) int {
+func sort2Disk(r io.Reader, memLimit int) int {
 	h := new(sortWords)
 	h.init(memLimit)
 	var ord int64
@@ -243,8 +317,14 @@ func (h *streamAggregator) Pop() interface{} {
 	return x
 }
 
-func merger(parts int) chan entry {
-	ch := make(chan entry, 4096)
+type countedEntry struct {
+	str string
+	ord int64
+	cnt int64
+}
+
+func merger(parts int) chan countedEntry {
+	ch := make(chan countedEntry, 4096)
 	go func() {
 		files := make([]*os.File, parts)
 		h := new(streamAggregator)
@@ -263,7 +343,7 @@ func merger(parts int) chan entry {
 			sr := heap.Pop(h).(*streamReader)
 			ord, _ := strconv.ParseInt(sr.ord, 10, 64)
 			cnt, _ := strconv.ParseInt(sr.cnt, 10, 64)
-			ch <- entry{sr.str, ord, cnt}
+			ch <- countedEntry{sr.str, ord, cnt}
 			if sr.next() {
 				heap.Push(h, sr)
 			}
@@ -282,7 +362,7 @@ func merger(parts int) chan entry {
 
 // findUnique reads from r with a specified bufsize
 // and trys to find the first unique string in this file
-func findUnique(r io.Reader, memLimit int64) {
+func findUnique(r io.Reader, memLimit int) {
 	// step.1 sort into file chunks
 	parts := sort2Disk(r, memLimit)
 	log.Println("generated", parts, "parts")
